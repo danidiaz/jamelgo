bed9e6d386393e0fd1e4b41cb48f8bcf9dd8ac88
Daniel Diaz <diaz.carrete@facebook.com>
2014-04-03 23:14:21 +0200

    OS

--- src/Snap/Snaplet/Jamelgo.hs
+++ src/Snap/Snaplet/Jamelgo.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE MultiWayIf #-}
 
 module Snap.Snaplet.Jamelgo where
 
@@ -13,7 +14,9 @@ import Control.Lens
 import Control.Exception (throwIO)
 import Data.Monoid
 import Data.Distributive
+import Data.Char
 import Data.Map
+import qualified Data.Map as M
 import Data.Maybe
 import Data.Aeson
 import Data.Traversable
@@ -24,17 +27,41 @@ import System.Directory
 
 import Snap.Snaplet
 
+data OS = Linux
+        | Windows
+
 newtype JavaExe = JavaExe FilePath
 
 data Jamelgo = Jamelgo
-    {   _jres :: Map T.Text JavaExe
+    {   _os :: OS
+    ,   _jres :: Map T.Text JavaExe
     ,   _servers :: Map T.Text FilePath
     }
 
 eitherDecodeFromFile :: (Functor m,MonadIO m,FromJSON j)
-                     => FilePath -> ErrorT String m (Map T.Text j)
+                     => FilePath -> ErrorT String m j
 eitherDecodeFromFile =
     liftIO . B.readFile >=> ErrorT . return . eitherDecodeStrict'
+
+-- note :: Monad m => e -> Maybe a -> ErrorT e m a
+-- note e = ErrorT . return . maybe (Left e) Right 
+
+(???) :: Monad m => Maybe a -> e -> ErrorT e m a
+(???) m e = ErrorT . return . maybe (Left e) Right $ m
+
+loadOS :: FilePath  
+       -> ErrorT String (Initializer b v) OS
+loadOS file = do
+    path <- (</> file) <$> lift getSnapletFilePath 
+    lift $ printInfo $ 
+        "Loading " <> T.pack file <> " from " <> T.pack path
+    singletonMap <- eitherDecodeFromFile path
+    str <- fmap T.toLower $ 
+        M.lookup ("os"::T.Text) singletonMap ??? "No os entry!" 
+    if | str == "windows" -> return Windows
+       | str == "linux" -> return Linux
+       | otherwise -> throwError $ 
+            "Unable to determine OS from string: " <> show str
 
 loadMap :: (FromJSON j) 
         => (j -> ErrorT String (Initializer b v) r)
@@ -46,7 +73,9 @@ loadMap traversal file = do
             "Loading " <> T.pack file <> " from " <> T.pack path
         eitherDecodeFromFile path >>= traverse traversal 
 
-findJavaExecutable :: MonadIO m => FilePath -> ErrorT String m JavaExe
+findJavaExecutable :: (MonadIO m) 
+                   => FilePath 
+                   -> ErrorT String m JavaExe
 findJavaExecutable path = do
     list <- liftIO (filterM doesFileExist files)
     case list of
@@ -59,9 +88,10 @@ jamelgoInit :: SnapletInit b Jamelgo
 jamelgoInit  = do
     makeSnaplet "jamelgo" "Jamelgo Snaplet" Nothing $ do
         result <- runErrorT $ do
+            theOS <- loadOS "OS.js"
             javaMap <- loadMap findJavaExecutable "JREs.js"
             serverMap <- loadMap return "servers.js"
-            return $ Jamelgo javaMap serverMap
+            return $ Jamelgo theOS javaMap serverMap
         either (liftIO . throwIO . userError) return $ result
 
 --
